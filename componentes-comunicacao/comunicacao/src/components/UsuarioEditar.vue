e1660a2a5decff8cab195b9a8013904ef41ae1b4
Diego Takahashi <diego.taka@hotmail.com>
2021-07-12 11:00:14 -0300

    aulasCursoVueJS

---
+++ componentes-comunicacao/comunicacao/src/components/UsuarioEditar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="alterarIdade">Alterar idade</button>
+        <!-- <button @click="reiniciarI()">Reiniciar idade</button> -->
+    </div>
+</template>
+
+<script>
+import barramento from '@/barramento'
+
+export default {
+    props: {
+        idade: Number,
+        // reiniciarI: Function
+    },
+    methods: {
+        alterarIdade(){
+            this.idade = 33
+            barramento.$emit('idadeMudou', this.idade)
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>
---
+++ dom/desafio-01/app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el:'#desafio',
+    data:{
+        nome: 'Diego',
+        idade: 21,
+        imagem: 'https://lolstatic-a.akamaihd.net/frontpage/apps/prod/rg-champion-aatrox/pt_BR/5b922bef08881410f8fffa7273c30a75dfb1d11f/assets/img/content/splash.jpg'
+
+    }
+})
